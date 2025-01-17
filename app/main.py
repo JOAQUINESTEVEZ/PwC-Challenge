9a0e6eeee3491c68c10bfc35e7a26bb3164525ba
Joaquin Estevez <105304562+JOAQUINESTEVEZ@users.noreply.github.com>
2025-01-17 09:27:53 -0600

    refactor: add __name__ entry for deploy

--- app/main.py
+++ app/main.py
@@ -49,3 +49,7 @@ def problem_details_handler(request, exc):
             "instance": str(request.url)
         }
     )
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
